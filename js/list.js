2ed9f0918143cd3087a03db876dc737b4b22ecd4
Bellox <billiabcd@gmail.com>
2022-08-14 17:29:40 +0200

    added files

---
+++ js/list.js
@@ -0,0 +1,83 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Shadow Rainn",
+			"author": "icedcave",
+			"more": "none",
+			"id": 57367243,
+			"percentToQualify": 129364,
+			"verificationVid": "https://www.youtube.com/watch?v=-qPWcBSUmyI",
+			"key": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Darkness is Coming",
+			"author": "Tablos AF",
+			"more": "none",
+			"id": 37364486,
+			"percentToQualify": 12937644,
+			"verificationVid": "https://www.youtube.com/watch?v=GJsfsykt8Ns",
+			"key": 1
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Violet Violence",
+			"author": "crystallizedz",
+			"more": "none",
+			"id": 82484165,
+			"percentToQualify": 238743,
+			"verificationVid": "https://www.youtube.com/watch?v=b7AWieAAvLA",
+			"key": 2
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "dzhemper",
+			"author": "sirwig",
+			"more": "none",
+			"id": 75813939,
+			"percentToQualify": 22334743,
+			"verificationVid": "https://www.youtube.com/watch?v=pvHC0ht2_z8",
+			"key": 3
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+
+      ],
+			"name": "Blood Cursed",
+			"author": "FJESTXYZ",
+			"more": "none",
+			"id": 81778059,
+			"percentToQualify": 2233422332743,
+			"verificationVid": "https://www.youtube.com/watch?v=X4IhO5QR1Is",
+			"key": 4
+		},
+		/*=================================================================================*/
+
+
+
+
+/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
